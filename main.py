0bca1623825d9ed827d306aa1164411b6edec6e2
SanamShivakoti <np03cs4m210078@heraldcollege.edu.np>
2025-03-09 22:39:25 +0545

    Exection logic for dump the csv file, required packages and APS failure training csv file

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 from sensor.exception import SensorException
 import os
 import sys
-
 from sensor.logger import logging
+from sensor.utils import dump_csv_file_to_mongodb_collection
+from sensor.constant.database import DATABASE_NAME, COLLECTION_NAME
+from sensor.constant.env_variables import FILE_PATH
+from dotenv import load_dotenv
 
 
-def test_exception():
-    try:
-        logging.info("Division by Zero")
-        a = 1/0
-    except Exception as e:
-        raise SensorException(e, sys)
+# def test_exception():
+#     try:
+#         logging.info("Division by Zero")
+#         a = 1/0
+#     except Exception as e:
+#         raise SensorException(e, sys)
 
 
 if __name__ == "__main__":
 
-    try:
-        test_exception()
-    except Exception as e:
-        print(e)
+    # try:
+    #     test_exception()
+    # except Exception as e:
+    #     print(e)
+    file_path = os.getenv(FILE_PATH)
+    database_name = DATABASE_NAME
+    collection_name = COLLECTION_NAME
+    dump_csv_file_to_mongodb_collection(
+        file_path, database_name, collection_name)
